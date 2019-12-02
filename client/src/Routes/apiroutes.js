fb85e0e3be9ff127deff4c475553d3c9c1161223
AaliyahM777 <liyah.34544@gmail.com>
2019-12-02 14:33:37 -0500

    added fixes

--- client/src/Routes/apiroutes.js
+++ client/src/Routes/apiroutes.js
@@ -1,17 +1,25 @@
 // how our react pages are connecting with our servers
    // here we use get products fucntion to fetch our products api
   const getProducts = () =>
-    fetch('http://localhost:5000/api/products')
+    // This is for working locally:
+    // fetch('http://localhost:5000/api/products')
+    //This is for working on Heroku:
+    fetch('https://penpowers.herokuapp.com/api/products')
 
 
 
 // here we fetch for our prices api we created which will include prices under 10 from database
 const getPrices = () =>
-fetch('http://localhost:5000/api/prices')
+// This is for working locally:
+// fetch('http://localhost:5000/api/prices')
+    //This is for working on Heroku:
+fetch('https://penpowers.herokuapp.com/api/prices')
 
 const getProductfilter = (category) =>
-    fetch(`http://localhost:5000/api/productfilter/${category}`)
-      
+    // This is for working locally:
+    // fetch(`http://localhost:5000/api/productfilter/${category}`)
+    //This is for working on Heroku:
+    fetch('https://penpowers.herokuapp.com/api/productfilter/${category}')
    export {getProducts, getProductfilter,getPrices}
 
 
